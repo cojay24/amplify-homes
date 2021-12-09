fe206da1b3a0347df232841a74eb640cfb252d9c
Scott Masson <scott.masson@openbrolly.com>
2021-12-09 16:13:56 +0000

    Card Visual fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { NewHomes, NavBar, MarketingFooter } from './ui-components'
+import { NewHomes, NavBar, MarketingFooter, Features2x2 } from './ui-components'
 
 function App() {
   return (
     <div className="App">
       <NavBar width={"100vw"} />
       <NewHomes isPaginated itemsPerPage={3} />
+      <Features2x2 />
       <MarketingFooter width={"100vw"} />
     </div>
   );
